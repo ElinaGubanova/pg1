d3c903666fc23a330f2a4ca0946bc5a37ad65dd7
ElinaGubanova <elina292017@gmail.com>
2024-11-10 15:33:27 +0300

    f1

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 45, 5 + 6, "red", True, 8 > 6, [3, 7, 89]
+print(immutable_var)
+#immutable_var [2] = 5 #кортеж не поддерживает обращение по элементам  и не изменяет элемнты
+
+mutable_list = [2, 5, 23, 1956, 545, 5778]
+print(mutable_list)
+mutable_list[-1] = 3466
+print(mutable_list)
