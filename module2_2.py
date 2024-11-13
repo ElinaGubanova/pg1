7f7c0d067515f220ba668309e285f4c8d35046ce
ElinaGubanova <elina292017@gmail.com>
2024-11-13 10:30:26 +0300

    f1

--- module2_2.py
+++ module2_2.py
@@ -3,7 +3,7 @@ second = input('Введите второе число')
 third = input('Введите третье число')
 if first != second and first != third and second != third:
     print(0)
-elif first or second == third:
-    print(2)
 elif first == second == third:
     print(3)
+elif first or second == third:
+    print(2)
