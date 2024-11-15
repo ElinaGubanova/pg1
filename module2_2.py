fa9d997554384cc3d1cf5b462ac60c463f486899
ElinaGubanova <elina292017@gmail.com>
2024-11-15 20:45:07 +0300

    f1

--- module2_2.py
+++ module2_2.py
@@ -1,9 +1,10 @@
-first = input('Введите первое число')
-second = input('Введите второе число')
-third = input('Введите третье число')
-if first != second and first != third and second != third:
-    print(0)
-elif first == second == third:
+first = int(input('Введите первое число'))
+second = int(input('Введите второе число'))
+third = int(input('Введите третье число'))
+
+if first == second == third:
     print(3)
-elif first or second == third:
+elif first == second or first == third or second == third:
     print(2)
+else:
+    print(0)
