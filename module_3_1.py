61d6e9fbb0f8a4341c8302c3abda66b26f9265ad
ElinaGubanova <elina292017@gmail.com>
2024-11-20 16:54:49 +0300

    f1

---
+++ module_3_1.py
@@ -0,0 +1,21 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains(string, list_to_search):
+    count_calls()
+    lowercase_string = string.lower()
+    return lowercase_string in (item.lower() for item in list_to_search)
+
+print(string_info('Develop'))
+print(string_info('meaning'))
+print(is_contains('Dream', ['eam', 'DrArA', 'drEAM', 'DrEam', 'Drean']))
+print(is_contains('Clothes', ['clothing', 'Disclothes']))
+
+print(calls)
