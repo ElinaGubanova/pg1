9a046ad4140388f012466def17f47fc3fcec2b01
ElinaGubanova <elina292017@gmail.com>
2024-12-03 16:34:39 +0300

    f2

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,22 +1,17 @@
 def calculate_structure_sum(data):
     element_sum = 0
 
-    for i in data:
-        if isinstance(i, (int, float)):
-            element_sum += i
-        elif isinstance(i, str):
-            element_sum += len(i)
-        elif isinstance(i, list):
-            element_sum += calculate_structure_sum(i)
-        elif isinstance(i, tuple):
-            element_sum += calculate_structure_sum(i)
-        elif isinstance(i, dict):
-            element_sum += calculate_structure_sum(i.items())
-        elif isinstance(i, set):
-            element_sum += calculate_structure_sum(i)
+    for elem in data:
+        if isinstance(elem, (list, tuple, set)):
+            element_sum += calculate_structure_sum(elem)
+        elif isinstance(elem, dict):
+            element_sum += calculate_structure_sum(elem.items())
+        elif isinstance(elem, (int, float)):
+            element_sum += elem
+        elif isinstance(elem, str):
+            element_sum += len(elem)
 
     return element_sum
-
 
 data_structure = [
     [1, 2, 3],
